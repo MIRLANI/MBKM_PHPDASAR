f02ae501cc90b391e874e744bc9862fb3fbda553
Muhammad Mirlani <laniwambuliga@gmail.com>
2023-03-27 11:51:43 +0800

    update materi looping

---
+++ file9.php
@@ -0,0 +1,46 @@
+<?php 
+
+
+// menggunakan for increment
+for ($i = 1; $i <= 10; $i++) {
+     echo "Bilangan ke $i <br>";
+}
+
+
+echo "<hr>";
+
+
+// menggunakan fot decroment
+for ($y=10; $y>=1; $y--) { 
+    echo "Bilangan ke $y <br>";
+}
+
+
+echo "<hr>";
+
+// menggunakan while increment
+$x= 1;
+while ($x <= 10) {
+    echo "ini menggunakan while ke $x <br>";
+    $x++;
+}
+
+echo "<hr>";
+// menggunakan while decrement
+$d= 10;
+while ($d >= 1) {
+    echo "ini menggunakan while ke $d <br>";
+    $d--;
+}
+
+
+// menggunakan do while
+$j = 1;
+do{
+    echo "bilangan ke $i <br>";
+    $j++;
+
+}while( $j<=10 );
+
+
+?>
